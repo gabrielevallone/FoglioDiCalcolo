5d39c59f0f9c3bf6f0e64e30f403f8ca96be3b25
vallone <gabriele.vallone@stud.unifi.it>
2022-09-02 16:42:24 +0200

    Creazione dei file header Observer e Subject.

---
+++ Observer.h
@@ -0,0 +1,14 @@
+//
+// Created by vallons on 02/09/22.
+//
+
+#ifndef FOGLIODICALCOLO_OBSERVER_H
+#define FOGLIODICALCOLO_OBSERVER_H
+
+class Observer {
+public:
+
+
+};
+
+#endif //FOGLIODICALCOLO_OBSERVER_H
---
+++ Subject.h
@@ -0,0 +1,14 @@
+//
+// Created by vallons on 02/09/22.
+//
+
+#ifndef FOGLIODICALCOLO_SUBJECT_H
+#define FOGLIODICALCOLO_SUBJECT_H
+
+#include "Observer.h"
+
+class Subject {
+
+};
+
+#endif //FOGLIODICALCOLO_SUBJECT_H
