2bdde80439be386b22f880033632550da46396f0
vallone <gabriele.vallone@stud.unifi.it>
2022-09-02 17:28:54 +0200

    Creazione classe Media per l'implementazione della funzione di media aritmetica.

---
+++ Media.h
@@ -0,0 +1,18 @@
+//
+// Created by vallons on 02/09/22.
+//
+
+#ifndef FOGLIODICALCOLO_MEDIA_H
+#define FOGLIODICALCOLO_MEDIA_H
+
+#include "Observer.h"
+#include "FoglioDiCalcolo.h"
+
+class Media : public Observer {
+public:
+    
+
+};
+
+
+#endif //FOGLIODICALCOLO_MEDIA_H
