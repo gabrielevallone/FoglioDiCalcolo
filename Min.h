dbc9ce32f25f2ddaf2e42a9d836cc8983d2aad3c
vallone <gabriele.vallone@stud.unifi.it>
2022-09-02 17:26:34 +0200

    Creazione classe Min per l'implementazione della funzione di minimo.

---
+++ Min.h
@@ -0,0 +1,17 @@
+//
+// Created by vallons on 02/09/22.
+//
+
+#ifndef FOGLIODICALCOLO_MIN_H
+#define FOGLIODICALCOLO_MIN_H
+
+#include "Observer.h"
+#include "FoglioDiCalcolo.h"
+
+class Min : public Observer {
+public:
+    
+};
+
+
+#endif //FOGLIODICALCOLO_MIN_H
