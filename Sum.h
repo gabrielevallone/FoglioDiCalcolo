2700c9b1f29b1917b96edaf1342ae3547ab0ecf3
vallone <gabriele.vallone@stud.unifi.it>
2022-09-02 17:27:56 +0200

    Creazione classe Sum per l'implementazione della funzione di somma.

---
+++ Sum.h
@@ -0,0 +1,17 @@
+//
+// Created by vallons on 02/09/22.
+//
+
+#ifndef FOGLIODICALCOLO_SUM_H
+#define FOGLIODICALCOLO_SUM_H
+
+#include "Observer.h"
+#include "FoglioDiCalcolo.h"
+
+class Sum : public Observer {
+public:
+
+};
+
+
+#endif //FOGLIODICALCOLO_SUM_H
