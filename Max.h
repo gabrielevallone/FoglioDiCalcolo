df06d18b74456bbd7494816a38a8df1a2f9b8360
vallone <gabriele.vallone@stud.unifi.it>
2022-09-02 17:01:19 +0200

    Creazione classe Max per l'implementazione della funzione di massimo

---
+++ Max.h
@@ -0,0 +1,17 @@
+//
+// Created by vallons on 02/09/22.
+//
+
+#ifndef FOGLIODICALCOLO_MAX_H
+#define FOGLIODICALCOLO_MAX_H
+
+#include "Observer.h"
+#include "FoglioDiCalcolo.h"
+
+class Max : public Observer {
+public:
+
+};
+
+
+#endif //FOGLIODICALCOLO_MAX_H
