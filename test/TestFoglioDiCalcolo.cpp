128aa01926ea902c906861d794d5e2b0e390070c
vallone <gabriele.vallone@stud.unifi.it>
2022-09-05 13:33:50 +0200

    Implementato test per le celle e il test per l'Observer su TestFoglioDiCalcolo.cpp

--- test/TestFoglioDiCalcolo.cpp
+++ test/TestFoglioDiCalcolo.cpp
@@ -35,3 +35,46 @@ TEST(FoglioDiCalcoloSuite, testingEvent) {
     ASSERT_DOUBLE_EQ(-1000, d);
 }
 
+
+TEST(FoglioDiCalcoloSuite, testingCellsValues) {
+
+    auto ptr = dynamic_cast<FoglioDiCalcolo *> (wxTheApp->GetTopWindow());
+    ptr->getCells()[0]->SetValue(wxT("14"));
+    ptr->getCells()[1]->SetValue(wxT("-7"));
+
+    ASSERT_DOUBLE_EQ(14, ptr->getValues()[0].value);
+    ASSERT_FALSE(ptr->getValues()[0].isEmpty);
+
+    ASSERT_DOUBLE_EQ(-7, ptr->getValues()[1].value);
+    ASSERT_FALSE(ptr->getValues()[1].isEmpty);
+
+    ptr->getCells()[0]->SetValue(wxEmptyString);
+    ASSERT_TRUE(ptr->getValues()[0].isEmpty);
+    ASSERT_TRUE(ptr->getValues()[2].isEmpty);
+}
+
+
+TEST(FoglioDiCalcoloSuite, testingOserverList) {
+
+    auto ptr = dynamic_cast<FoglioDiCalcolo *> (wxTheApp->GetTopWindow());
+    unsigned long int l = ptr->getObserverList().size();
+    ASSERT_EQ(0, l);
+
+    auto sum = new Sum(ptr);
+    auto max = new Max(ptr);
+    auto min = new Min(ptr);
+    auto media = new Media(ptr);
+    l = ptr->getObserverList().size();
+    ASSERT_EQ(5, l);
+
+    delete sum;
+    delete max;
+    l = ptr->getObserverList().size();
+    ASSERT_EQ(3, l);
+
+    delete min;
+    delete media;
+    l = ptr->getObserverList().size();
+    ASSERT_EQ(0, l);
+
+}
